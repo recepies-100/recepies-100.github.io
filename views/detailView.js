25acfc3d01a83c42252e021dafcc71e576c22fb9
Velin Dankov <velin80@web.de>
2024-08-26 20:54:18 +0300

    add details some functionality

--- views/detailView.js
+++ views/detailView.js
@@ -26,22 +26,17 @@ const detailsTemp = (data) => html `
       <div class="details-ingredients">
         <h2 id="details-title__sec">Ingrediens</h2>
         <ul class="ingredients-list">
-          <li class="ingredients-list__item">3-4 eggs</li>
-          <li class="ingredients-list__item">salz & pfeffer</li>
-          <li class="ingredients-list__item">1 spoon butter or oil</li>
-          <li class="ingredients-list__item">optional feelng </li>
+          <li class="ingredients-list__item">${data.ingrediance
+          }</li>
+          
         </ul>
       </div>
       <div class="details-instructions">
         <h2 id="details-title__sec">Instructions</h2>
-        <ol class="instruction-list">
-          <li class="instruction-list__item">Beat the eggs</li>
-          <li class="instruction-list__item">Heat a pan</li>
-          <li class="instruction-list__item">Cook the Omlette</li>
-          <li class="instruction-list__item">Add fillings</li>
-          <li class="instruction-list__item">Fold and serve</li>
-          <li class="instruction-list__item">Enjoy</li>
-        </ol>
+        <ul class="instruction-list">
+          <li class="instruction-list__item">${data.description}</li>
+          
+        </ul>
       </div>
       <div class="details-btns">
         <button id="delete">Delete Reciepe</button>
@@ -55,6 +50,6 @@ export async function showDetailsView(ctx) {
   const id = ctx.params.id;
   const data = await getReciepeById(id);
 
-
+debugger
   ctx.render(detailsTemp(data))
 }
